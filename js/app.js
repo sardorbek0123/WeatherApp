05d1a912c9b98a0c8f170cface67228224d75c09
Sardorbek <jabborovsardor21@gmail.com>
2023-04-01 11:36:16 +0500

    change background and theme

--- js/app.js
+++ js/app.js
@@ -22,7 +22,12 @@ const updateUI = (weather) => {
   card.children[0].children[0].style.display = "inline";
   details.innerHTML = `
     <h5 class="mb-3">${weather.name}, ${weather.sys.country}</h5>
-    <p class="mb-3">${weather.weather[0].main}</p>
+    <p class="mb-3 hum__speed">${weather.weather[0].main}</p>
+    <p class="mb-3">Humidity : <span class="hum__speed">${
+      weather.main.humidity
+    }%</span> <br/> Wind Speed : <span class="hum__speed">${
+    weather.wind.speed
+  }km/hr</span></p>
     <div class="display-4 mb-3">
      <span>${Math.round(weather.main.temp)}</span>
      <span>&deg;C</span>
@@ -35,30 +40,27 @@ const updateUI = (weather) => {
 
   weatherIcon.src = `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`;
 
-  console.log(weather.weather[0].main);
-
   if (weather.weather[0].main === "Clear") {
     body.style.backgroundImage = "url(images/clear.jpg)";
-    console.log("CLEAR");
   } else if (weather.weather[0].main === "Rain") {
     body.style.backgroundImage = "url(images/rain.jpg)";
-    console.log("RAIN");
   } else if (weather.weather[0].main === "Clouds") {
     body.style.backgroundImage = "url(images/clouds.jpg)";
-    console.log("CLOUDS");
   } else if (weather.weather[0].main === "Snow") {
     body.style.backgroundImage = "url(images/snow.jpg)";
-    console.log("SNOWING");
+  } else if (weather.weather[0].main === "Drizzle") {
+    body.style.backgroundImage = "url(images/drizzle.jpg)";
   }
 };
 
 // ERROR
 const error = () => {
   card.children[0].children[0].style.display = "none";
-  details.innerHTML = `<h5 class="mb-3">Location is not found. Please try again</h5>`;
+  details.innerHTML = `<h5 class="mb-3 mt-3">Location not found. Please try again</h5>`;
   if (card.classList.contains("d-none")) {
     card.classList.remove("d-none");
   }
+  body.style.backgroundImage = "url(images/default.jpg)";
 };
 
 // get weather from API
